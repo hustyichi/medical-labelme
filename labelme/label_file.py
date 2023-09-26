30891796ea83d8c8464d1cd40c14aed046a9fd95
libing <libing@healink.cn>
2023-09-26 15:15:11 +0800

    ADD: add FrameLabel and ImageLabel

--- labelme/label_file.py
+++ labelme/label_file.py
@@ -3,6 +3,7 @@ import contextlib
 import io
 import json
 import os.path as osp
+from typing import Optional, List
 
 import numpy as np
 import PIL.Image
@@ -13,6 +14,7 @@ from labelme.logger import logger
 from labelme import PY2
 from labelme import QT4
 from labelme import utils
+from labelme.shape import Shape
 
 
 PIL.Image.MAX_IMAGE_PIXELS = None
@@ -34,8 +36,128 @@ class LabelFileError(Exception):
     pass
 
 
-class LabelFile(object):
+# the mininal image unit
+class FrameLabel(object):
+    def __init__(
+        self,
+        filename: str,
+        image_pil: PIL.Image.Image,
+        frame: int = 0,
+        image_data: Optional[bytes] = None,
+        shapes: Optional[List[Shape]] = None,
+    ) -> None:
+        self.filename = filename
+        self.frame = frame
+        self.image_data_bytes = image_data
+        self.image_pil = image_pil
+        self.shapes = shapes or []
 
+    @property
+    def image_data(self) -> bytes:
+        if self.image_data_bytes:
+            return self.image_data_bytes
+
+        self.image_data_bytes = utils.preprocess_img(self.image_pil, self.filename)
+        return self.image_data_bytes
+
+    def add_shape(self, shape: Shape):
+        self.shapes.append(shape)
+
+    def remove_shapes(self, shapes: List[Shape]):
+        for shape in shapes:
+            self.shapes.remove(shape)
+
+    def format(self):
+        return {
+            "frame": self.frame,
+            "shapes": [shape.format() for shape in self.shapes],
+            "imageWidth": self.image_pil.width,
+            "imageHeight": self.image_pil.height,
+        }
+
+
+class ImageLabel(object):
+    suffix = "*.json"
+
+    def __init__(self, filename: Optional[str] = None) -> None:
+        self.filename = filename
+        self.frame_labels: List[FrameLabel] = []
+        self.current_frame = 0
+        self.total_frame = 0
+        self.flags: dict = {}
+
+        if self.filename:
+            self.load(self.filename)
+
+    def load_image(self, image_path: str):
+        frame = 0
+        for image_pil in utils.load_image(image_path):
+            self.frame_labels.append(FrameLabel(image_path, image_pil, frame=frame))
+            frame += 1
+
+        self.filename = image_path
+        self.total_frame = len(self.frame_labels)
+
+    def next_frame(self):
+        if self.current_frame >= self.total_frame - 1:
+            return False
+
+        self.current_frame += 1
+        return True
+
+    def prev_frame(self):
+        if self.current_frame <= 0:
+            return False
+
+        self.current_frame -= 1
+        return True
+
+    @property
+    def current_frame_label(self) -> Optional[FrameLabel]:
+        if self.current_frame >= len(self.frame_labels):
+            return None
+
+        return self.frame_labels[self.current_frame]
+
+    @property
+    def current_frame_image_data(self) -> Optional[bytes]:
+        current_label = self.current_frame_label
+        if not current_label:
+            return None
+
+        return current_label.image_data
+
+    # load a image file or label file
+    def load(self, filename: str):
+        if utils.is_supported_image(filename):
+            self.load_image(filename)
+
+    def format(self):
+        return dict(
+            version=__version__,
+            flags=self.flags,
+            imagePath=self.filename,
+            totalFrames=self.total_frame,
+            frames=[frame.format() for frame in self.frame_labels if frame.shapes],
+        )
+
+    # save image label to local file
+    def save(self, filename: str, other_data: Optional[dict] = None):
+        data = other_data or {}
+        data.update(self.format())
+
+        try:
+            with open(filename, "w") as f:
+                json.dump(data, f, ensure_ascii=False, indent=2)
+        except Exception as e:
+            raise LabelFileError(e)
+
+    @staticmethod
+    def is_label_file(filename):
+        return osp.splitext(filename)[1].lower() == ImageLabel.suffix
+
+
+class LabelFile(object):
     suffix = ".json"
 
     def __init__(self, filename=None):
